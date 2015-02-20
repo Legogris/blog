d62fd9ab322b83b488a1355438987c0117fed182
Robert Edstrom <legogris@legogris.se>
2015-02-21 02:29:10 +0900

    Can select by year

--- services/PostService.js
+++ services/PostService.js
@@ -8,7 +8,11 @@ var pages = {
 module.exports = {
 	name: 'post',
 	read: function(req, resource, params, config, cb) {
-		var data = [pages[params.slug]];
+		if(typeof params.slug !== 'undefined') {
+			var data = [pages[params.slug]];
+		} else if(typeof params.year !== 'undefined') {
+			var data = [pages.hej, pages.hopp];
+		}
 		console.log('FETCH DATA', resource, params, config, data);
 		cb(null, data);
 	}
