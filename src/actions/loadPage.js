1fb6dbd3c8c9b783373a5297d3542c31e71fa85f
Robert Edstrom <legogris@legogris.se>
2015-02-23 00:09:13 +0900

    Remove obsolete 'dynamic' load

--- src/actions/loadPage.js
+++ src/actions/loadPage.js
@@ -25,15 +25,6 @@ module.exports = {
 		});
 	    context.dispatch('UPDATE_PAGE_TITLE', payload.config.title);
 	},
-	dynamic: function(context, payload, done) {
-	    context.dispatch('LOAD_PAGE', {
-	        id: payload.params.title
-	    });
-	    context.dispatch('UPDATE_PAGE_TITLE', {
-	        pageTitle: payload.config.title
-	    });
-	    done();
-	},
 	post: function(context, payload, done) {
 		context.service.read('post', {slug: payload.params.slug}, {}, loadPosts(context, done));
 	},
