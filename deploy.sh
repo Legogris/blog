1fd02e6b1fbbb479091e0c7084be3842d2d09db4
Robert Edstrom <legogris@legogris.se>
2015-02-27 01:35:11 +0900

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#Usage: deploy.se HOST PORT
+npm run deploy && docker build -t blog . &&
+echo "Saving"
+docker save blog > blog.tar
+echo "Copying to $1"
+scp blog.tar $1:
+echo "Deploying"
+ssh $1 "docker load < blog.tar && docker stop blog && docker rm blog && docker run -d --link mongo:mongo -p $2:80 --name=blog blog && mv blog.tar blog.prev.tar"
