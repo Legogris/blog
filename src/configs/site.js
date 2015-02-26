7072b17c016a5cc837d3b31e96bf33daa1b79a05
Robert Edstrom <legogris@legogris.se>
2015-02-26 23:58:19 +0900

    Adding forgot files from previous commits: Feed generator, icon and site config

---
+++ src/configs/site.js
@@ -0,0 +1,6 @@
+module.exports = {
+	url: 'http://legogris.se/',
+	title: 'Den of Lego',
+	author: 'Robert Edstrom',
+	description: 'I post stuff on <a href="/food">Vegan&nbsp;cooking&nbsp;in&nbsp;Japan</a>,<br /><a href="/hacking">programming</a> and<br /><a href="/sundry">other stuff</a>.'
+}
---
+++ src/feed.js
@@ -0,0 +1,41 @@
+const RSS = require('rss');
+const Post = require('./models/post');
+const Term = require('./models/term');
+const site = require('./configs/site');
+
+Feed = {
+	generate: function(category, itemCount) {
+		return Promise.all([
+			Term.findOne({slug: category}).exec(), 
+			Post.find({cat: category}).exec()])
+		.then(result  => {
+			cat = result[0];
+			posts = result[1];
+			var feed = new RSS({
+				title: site.title + ' - ' + cat.title,
+				description: cat.description,
+				feed_url: site.url + 'feed/' + category + '/',
+				site_url: site.url + category + '/',
+				categories: [cat.title],
+				author: site.author
+			});
+			posts.forEach(post => {
+				feed.item({
+					title: post.title,
+					description: post.content, //TODO: Transform links to be absolute, maybe trim
+					url: post.absoluteURI,
+					guid: post._id,
+					categories: post.terms,
+					date: new Date(post.time),
+				});
+			});
+			var xml = feed.xml({indent: true});
+			return xml;
+		}, err => {
+			console.error('FEED ERROR: ', err)
+			throw err;
+		});
+	}
+}
+	
+module.exports = Feed;
