21073487ad74b410ebd949597c83b23309984762
Robert Edstrom <legogris@legogris.se>
2015-06-17 22:58:38 +0900

    Deploy info to docker-compose.prod

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ docker save blog > blog.tar
 echo "Copying to $1"
 scp blog.tar $1:
 echo "Deploying"
-ssh $1 "docker load < blog.tar && docker stop blog && docker rm blog && docker run -d --link mongo:mongo -p $2:80 --name=blog blog && mv blog.tar blog.prev.tar"
+ssh $1 "docker load < blog.tar && docker stop blog && docker rm blog && docker run -d --link blog_mongo:mongo -p $2:80 --name=blog blog && mv blog.tar blog.prev.tar"
